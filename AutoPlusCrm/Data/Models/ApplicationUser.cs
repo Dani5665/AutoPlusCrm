2465bfb9fe1caf7d427e8b942f9ddc82dad33bdc
Yordan Vuchkov <yordan5665@gmail.com>
2024-04-02 15:41:43 +0300

    Add IsActive field to ApplicationUser

--- AutoPlusCrm/Data/Models/ApplicationUser.cs
+++ AutoPlusCrm/Data/Models/ApplicationUser.cs
@@ -11,5 +11,7 @@ namespace AutoPlusCrm.Data.Models
         public int UserStoreId { get; set; }
 
         public RetailerStores UserStore { get; set; } = null!;
+
+        public bool? IsActive { get; set; }
     }
 }
